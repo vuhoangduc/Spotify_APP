c145b7c866ecacb818eaace48678cd27058cb35e
vuhoangduc <ttgvhd@gmail.com>
2022-12-06 14:14:30 +0700

    update code 6/12

--- app/src/main/java/com/example/duan1_spotify_clone/DanhSachNhac/FragmentDanhSachNhac.java
+++ app/src/main/java/com/example/duan1_spotify_clone/DanhSachNhac/FragmentDanhSachNhac.java
@@ -62,6 +62,6 @@ public class FragmentDanhSachNhac extends Fragment {
         Dont_Open dont_open = new Dont_Open(getActivity());
         JsonParser_DanhMuc jsonParser = new JsonParser_DanhMuc(getActivity(),recyclerView,dont_open.getData());
 //        Log.d("zzzzzzzzzzzzzzzzzzzzzzzzzzzzz"+getMachineIP(), "init: ");
-        jsonParser.execute("http://10.24.15.50:3000/danhmucs");
+        jsonParser.execute("http://192.168.0.104:3000/danhmucs");
     }
 }
